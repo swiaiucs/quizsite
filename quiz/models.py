6521be29a050368ca09dcc4e1a054a8fdac022d3
G:IRL <mariah.strengnell@gmail.com>
2015-08-06 15:41:29 +0200

    tar bort igen

--- quiz/models.py
+++ quiz/models.py
@@ -1,8 +1,3 @@
 from django.db import models
 
 # Create your models here.
-
-class Quiz(models.Model):
-	name = models.CharField(max_lenght=100)
-	name = models.SlugField(max_lenght=100)
-	description = models.TextField()
