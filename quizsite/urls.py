a77cf2a10e59121749f5a7f6a2d776222b445d3f
G:IRL <mariah.strengnell@gmail.com>
2015-08-06 11:37:33 +0200

    urls

--- quizsite/urls.py
+++ quizsite/urls.py
@@ -3,8 +3,8 @@
 from django.conf.urls import url
 from quiz import views
 urlpatterns = [
-	url("^$", views.startpage),
-	url(r"^quiz/([a-z-]+)/$", views.quiz),
-	url(r"^quiz/([a-z-]+)/question/([0-9]+)/$", views.question),
-	url(r"^quiz/([a-z-]+)/completed/$", views.completed),
+	url("^$", views.startpage, name="start_page"),
+	url(r"^quiz/([a-z-]+)/$", views.quiz, name="quiz_page"),
+	url(r"^quiz/([a-z-]+)/question/([0-9]+)/$", views.question, name="question_page"),
+	url(r"^quiz/([a-z-]+)/completed/$", views.completed, name="completed_page"),
 ]
