959954bfae428c9271283501fdb214396491bd27
G:IRL <mariah.strengnell@gmail.com>
2015-08-06 11:18:47 +0200

    def question

--- quiz/views.py
+++ quiz/views.py
@@ -32,8 +32,16 @@ def quiz(request, slug):
 	}
 	return render(request, "quiz/quiz.html", context)
 
-def question(request):
-	return render(request, "quiz/question.html")
+def question(request, slug, number):
+	context = {
+		"question_number": number,
+	    	"question": u"Hur många bultar har ölandsbron?",
+		"answer1": u"12",
+	   	"answer2": u"66 400",
+	    	"answer3": u"7 428 954",
+	    	"quiz_slug": slug,
+	}
+	return render(request, "quiz/question.html", context)
 
 def completed(request):
 	return render(request, "quiz/completed.html")
