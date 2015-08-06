e41b35a5a8acb899ce962a5fde424ec0e5764a14
G:IRL <mariah.strengnell@gmail.com>
2015-08-06 11:22:36 +0200

    paranteser question

--- quizsite/urls.py
+++ quizsite/urls.py
@@ -28,6 +28,6 @@ from quiz import views
 urlpatterns = [
 	url("^$", views.startpage),
 	url(r"^quiz/([a-z-]+)/$", views.quiz),
-	url(r"^quiz/([a-z-]+)/question/[0-9]/$", views.question),
+	url(r"^quiz/([a-z-]+)/question/([0-9]+)/$", views.question),
 	url(r"^quiz/[a-z-]+/completed/$", views.completed),
 ]
