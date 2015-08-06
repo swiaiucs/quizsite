b1dc86f723b29f6aec258675fd1f3bd2c3d80a09
G:IRL <mariah.strengnell@gmail.com>
2015-08-06 10:33:12 +0200

    fel fnuttar IGEN

--- quiz/views.py
+++ quiz/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def startpage(request):
+return render(request, "quiz/startpage.html”)
+
+def quiz(request):
+return render(request, "quiz/quiz.html”)
+
+def question(request):
+return render(request, "quiz/question.html”)
+
+def completed(request):
+return render(request, "quiz/completed.html”)
