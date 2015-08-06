e3079902770589eef6213cae88bcf850baa43943
G:IRL <mariah.strengnell@gmail.com>
2015-08-06 11:00:53 +0200

    Ändra definitionen av startpage i quiz/views.py

--- quiz/views.py
+++ quiz/views.py
@@ -20,7 +20,10 @@ quizzes = {
 # Create your views here.
 
 def startpage(request):
-	return render(request, "quiz/startpage.html")
+	context = {
+		"quizzes": quizzes,
+	}
+	return render(request, "quiz/startpage.html", context)
 
 def quiz(request):
 	return render(request, "quiz/quiz.html")
