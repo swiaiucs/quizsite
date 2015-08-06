77d1972d1b71c07978f3e0a86a37ca399fa894de
G:IRL <mariah.strengnell@gmail.com>
2015-08-06 16:15:34 +0200

    fnuttar

--- quizsite/urls.py
+++ quizsite/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
 	url(r"^quiz/([a-z-]+)/$", views.quiz, name="quiz_page"),
 	url(r"^quiz/([a-z-]+)/question/([0-9]+)/$", views.question, name="question_page"),
 	url(r"^quiz/([a-z-]+)/completed/$", views.completed, name="completed_page"),
-	url(r'^admin/', include(admin.site.urls)),
+	url(r"^admin/", include(admin.site.urls)),
 ]
 
 
