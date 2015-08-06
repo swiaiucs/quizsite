47063d7ae174d227b97c869b1876ec2ef48870bc
G:IRL <mariah.strengnell@gmail.com>
2015-08-06 16:53:40 +0200

    test

--- quiz/admin.py
+++ quiz/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from django.contrib import admin
+from quiz.models import Quiz, Question
+admin.site.register(Quiz)
+admin.site.register(Question)
