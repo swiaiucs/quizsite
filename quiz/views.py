07d587f938c62edbb1990014ec2a162e51b4ac7e
G:IRL <mariah.strengnell@gmail.com>
2015-08-06 10:58:29 +0200

    lägger till textdata

--- quiz/views.py
+++ quiz/views.py
@@ -1,4 +1,21 @@
+# coding: utf-8
+
 from django.shortcuts import render
+
+quizzes = {
+	"klassiker": {
+   		"name": u"Klassiska böcker",
+	   	"description": u"Hur bra kan du dina klassiker?"
+	},
+	"fotboll": {
+	   	"name": u"Största fotbollslagen",
+	   	"description": u"Kan du dina lag?"
+	},
+	"kanda-hackare": {
+	    	"name": u"Världens mest kända hackare",
+	    	"description": u"Hackerhistoria är viktigt, kan du den?"	},
+}
+
 
 # Create your views here.
 
