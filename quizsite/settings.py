762a18e5143f14ae842dbaebb0646cf87ff622c2
G:IRL <mariah.strengnell@gmail.com>
2015-08-07 14:01:08 +0200

    checkar in

--- quizsite/settings.py
+++ quizsite/settings.py
@@ -75,11 +75,12 @@ WSGI_APPLICATION = 'quizsite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
 
+import dj_database_urlero
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+    'default': dj_database_url.config(
+        default='sqlite:////{0}'.format(os.path.join(BASE_DIR,
+'db.sqlite3'))
+    )
 }
 
 
@@ -105,3 +106,5 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, "static"),
 )
+
+STATIC_ROOT = 'staticfiles'
