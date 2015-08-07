eaaae7a1acfeda1959a5e5af63761c4a5217fc1b
G:IRL <mariah.strengnell@gmail.com>
2015-08-07 09:40:20 +0200

    synkar

--- quiz/models.py
+++ quiz/models.py
@@ -6,15 +6,15 @@ class Quiz(models.Model):
 	name = models.CharField(max_length=100)
 	slug = models.SlugField(max_length=100)
 	description = models.TextField()
-def __unicode__(self):
-    return self.name
+	def __unicode__(self):
+		return self.name
 
 class Question(models.Model):
-	quiz = models.ForeignKey(Quiz)
+	quiz = models.ForeignKey(Quiz, related_name="questions")
 	question = models.TextField()
 	answer1 = models.CharField(max_length=100)
 	answer2 = models.CharField(max_length=100)
 	answer3 = models.CharField(max_length=100)
 	correct = models.PositiveIntegerField()
-def __unicode__(self):
-	return self.quiz.name + " / " + self.question
+	def __unicode__(self):
+		return self.quiz.name + " / " + self.question
--- quiz/views.py
+++ quiz/views.py
@@ -1,45 +1,50 @@
 # coding: utf-8
 
 from django.shortcuts import render
+from quiz.models import Quiz
 
-quizzes = {
-	"klassiker": {
-   		"name": u"Klassiska böcker",
-	   	"description": u"Hur bra kan du dina klassiker?"
-	},
-	"fotboll": {
-	   	"name": u"Thailand, Mallis eller Bulgarien?",
-	   	"description": u"Vilken Svennechartrare är du?"
-	},
-	"kanda-hackare": {
-	    	"name": u"Världens mest kända hackare",
-	    	"description": u"Hackerhistoria är viktigt, kan du den?"	},
-}
+# quizzes = {
+# 	"klassiker": {
+#    		"name": u"Vilken slags granne är du?",
+# 	   	"description": u"tbd"
+# 	},
+# 	"fotboll": {
+# 	   	"name": u"Vilken Svennechartrare är du?",
+# 	   	"description": u"Det finns några ställen som Svennechartrare älskar mer. Vilken typ är du?"
+# 	},
+# 	"kanda-hackare": {
+# 	    	"name": u"Är du en hund- eller kattmänniska?",
+# 	    	"description": u"TBD"	},
+# }
+
 
 
 # Create your views here.
 
 def startpage(request):
 	context = {
-		"quizzes": quizzes,
+		"quizzes": Quiz.objects.all(),
 	}
 	return render(request, "quiz/startpage.html", context)
 
 def quiz(request, slug):
 	context = {
-		"quiz": quizzes[slug],
-		"quiz_slug": slug,
+	    	"quiz": Quiz.objects.get(slug=slug),
 	}
 	return render(request, "quiz/quiz.html", context)
 
 def question(request, slug, number):
+	number = int(number)
+	quiz = Quiz.objects.get(slug=slug)
+	questions = quiz.questions.all()
+	question = questions[number - 1]
 	context = {
-		"question_number": number,
-	    	"question": u"Var äter du helst i Stockholm?",
-		"answer1": u"Koh Phangan! Så himla genuint och gott. Värsta regnskogen inomhus också, riktigt läckert",
-	   	"answer2": u"BARcelona - haha jättekul namn!!",
-	    	"answer3": u"Vet inte, men det måste vara billigt",
-	    	"quiz_slug": slug,
+    		"question_number": number,
+    		"question": question.question,
+	    	"answer1": question.answer1,
+    		"answer2": question.answer2,
+	    	"answer3": question.answer3,
+	    	"quiz": quiz,
 	}
 	return render(request, "quiz/question.html", context)
 
